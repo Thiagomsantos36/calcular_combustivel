40ed7b416b34cd02085bd7858b946be4c0e3e364
Yuri Fialho <yurirfialho@gmail.com>
2023-10-07 12:19:38 -0300

    Update buildapk.ps1
    Adicionado o prebuild

--- buildapk.ps1
+++ buildapk.ps1
@@ -1,6 +1,7 @@
 echo "Set Java JDK Versao 11"
 $env:JAVA_HOME = "C:\Program Files\Java\jdk-11"
 npm install
+npx expo prebuild
 npx react-native build-android --mode=release
 .\android\gradlew assembleRelease
 echo "Pegar o APK na pasta android/app/outputs/apk"
