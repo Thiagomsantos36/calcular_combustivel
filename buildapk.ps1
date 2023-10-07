07ceded82b46d2fe557924b76548adfbdc205f17
Yuri Fialho <yurirfialho@gmail.com>
2023-10-07 12:28:57 -0300

    Update buildapk.ps1

--- buildapk.ps1
+++ buildapk.ps1
@@ -3,5 +3,6 @@ $env:JAVA_HOME = "C:\Program Files\Java\jdk-11"
 npm install
 npx expo prebuild
 npx react-native build-android --mode=release
-.\android\gradlew assembleRelease
+# Executar o comando abaixo dentro da pasta do Android
+./android/gradlew assembleRelease
 echo "Pegar o APK na pasta android/app/outputs/apk"
